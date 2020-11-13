351e602b88e3d81fe2692d85b24a7c599e12b6d5
Aleš Knížek <knizek@spstrutnov.cz>
2020-11-13 10:52:45 +0100

    úpravy rozvrhu 2

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function oznacit(){
-    var oznaceni = false;
-
-    document.getElementsById("PO0").style.backgroundColor = "red";
+    var oznaceni = true;
+    if(oznaceni == true){
+        document.getElementById("PO0").style.backgroundColor = "red";
+        oznaceni = false;
+    }else if(oznaceni == false){
+        document.getElementById("PO0").style.backgroundColor = "#0080FF";
+        oznaceni = true;
+    }
 }
